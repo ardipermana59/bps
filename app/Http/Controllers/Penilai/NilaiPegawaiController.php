10503563c96a7d7aa4c5f27bec78639b9412234f
Ardi Permana <ardipermana59@gmail.com>
2022-08-08 23:06:56 +0700

    fix get data & create laporan pdf on page Nilai

--- app/Http/Controllers/Penilai/NilaiPegawaiController.php
+++ app/Http/Controllers/Penilai/NilaiPegawaiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Penilai;
 
 use App\Http\Controllers\Controller;
+use App\Models\Employee;
 use App\Models\Nilai;
 use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade\Pdf;
@@ -16,11 +17,17 @@ class NilaiPegawaiController extends Controller
      */
     public function index()
     {
+        $evaluator = Employee::where('user_id', auth()->user()->id)->first();
+
         $data = Nilai::join('ambil_kegiatans', 'ambil_kegiatans.id', 'nilais.ambil_kegiatan_id')
-        ->join('activities', 'activities.id', 'ambil_kegiatans.activity_id')
-        ->join('employees', 'employees.id', 'ambil_kegiatans.employee_id')
-        ->select('employees.full_name', 'activities.name as activity_name' , 'nilais.id', 'nilais.target_realisasi as target', 'nilais.kerjasama', 'nilais.ketepatan_waktu', 'nilais.kualitas')
-        ->get();
+            ->join('activities', 'activities.id', 'ambil_kegiatans.activity_id')
+            ->join('employees', 'employees.id', 'ambil_kegiatans.employee_id')
+            ->join('penilai_pegawais', 'penilai_pegawais.employee_id', 'employees.id')
+            ->join('evaluators', 'evaluators.id', 'penilai_pegawais.evaluator_id')
+            ->join('employees as pengawas', 'pengawas.id', 'evaluators.employee_id')
+            ->where('evaluators.employee_id', $evaluator->id)
+            ->select('employees.full_name', 'activities.name as activity_name', 'nilais.id', 'nilais.target_realisasi as target', 'nilais.kerjasama', 'nilais.ketepatan_waktu', 'nilais.kualitas')
+            ->get();
         return view('pages.penilai.pegawai.index', compact('data'));
     }
 
@@ -40,11 +47,11 @@ class NilaiPegawaiController extends Controller
             'ketepatan_waktu' => 'required|integer|min:1|max:100',
             'kualitas' => 'required|integer|min:1|max:100',
         ]);
-        
+
         $nilai = Nilai::find($request->id);
 
-        if($nilai == null){
-            return response()->json(['message'=>'Data tidak ditemukan'],404);
+        if ($nilai == null) {
+            return response()->json(['message' => 'Data tidak ditemukan'], 404);
         }
 
         $nilai->target_realisasi = $request->target;
@@ -53,12 +60,83 @@ class NilaiPegawaiController extends Controller
         $nilai->kualitas = $request->kualitas;
         $nilai->save();
 
-        return response()->json(['success'=>'Data berhasil disimpan']);;
+        return response()->json(['success' => 'Data berhasil disimpan']);;
     }
 
     public function exportPdf()
     {
-        $pdf = Pdf::loadView('pages.penilai.pegawai.pdf-template');
-        return $pdf->stream();
+        $evaluator = Employee::where('user_id', auth()->user()->id)->first();
+
+        $response = Nilai::join('ambil_kegiatans', 'ambil_kegiatans.id', 'nilais.ambil_kegiatan_id')
+            ->join('activities', 'activities.id', 'ambil_kegiatans.activity_id')
+            ->join('employees', 'employees.id', 'ambil_kegiatans.employee_id')
+            ->join('penilai_pegawais', 'penilai_pegawais.employee_id', 'employees.id')
+            ->join('evaluators', 'evaluators.id', 'penilai_pegawais.evaluator_id')
+            ->join('employees as pengawas', 'pengawas.id', 'evaluators.employee_id')
+            ->where('evaluators.employee_id', $evaluator->id)
+            ->select('pengawas.full_name as evaluator_name', 'employees.full_name', 'employees.nip', 'activities.name as activity_name', 'nilais.id', 'nilais.target_realisasi as target', 'nilais.kerjasama', 'nilais.ketepatan_waktu', 'nilais.kualitas')
+            ->orderBy('full_name')
+            ->get();
+
+        // cek datanya ada atau tidak
+        if (count($response) == 0) {
+            return redirect()->back()->with('error', 'Gagal export PDF karena data kosong');
+        }
+
+        $data = [];
+        foreach ($response as $key => $value) {
+
+            /**
+             * Buat data seperti ini
+             *   [
+             *     'employee_name' =>
+             *     'nip' =>
+             *     'evaluator_name' =>
+             *     'kegiatans' => [
+             *                    [
+             *                      'activity_name' =>
+             *                      'target' =>
+             *                      'kerjasama' =>
+             *                      'ketepatan_waktu' =>
+             *                      'kualitas' =>
+             *                    ] 
+             *   ] 
+             */
+            if (count($data) == 0) {
+                $data[]['employee_name'] = $value->full_name;
+                $dataIndex = count($data) - 1;
+                $data[$dataIndex]['nip'] = $value->nip;
+                $data[$dataIndex]['evaluator_name'] = $value->evaluator_name;
+                $data[$dataIndex]['activities'][]['activity_name'] = $value->activity_name;
+                $kegiatanIndex = count($data[$dataIndex]['activities']) - 1;
+                $data[$dataIndex]['activities'][$kegiatanIndex]['target'] = $value->target;
+                $data[$dataIndex]['activities'][$kegiatanIndex]['kerjasama'] = $value->kerjasama;
+                $data[$dataIndex]['activities'][$kegiatanIndex]['ketepatan_waktu'] = $value->ketepatan_waktu;
+                $data[$dataIndex]['activities'][$kegiatanIndex]['kualitas'] = $value->kualitas;
+            } else {
+                if ($value->nip == $response[$key - 1]->nip) {
+                    $data[$key - 1]['activities'][]['activity_name'] = $value->activity_name;
+                    $kegiatanIndex = count($data[$key - 1]['activities']) - 1;
+                    $data[$key - 1]['activities'][$kegiatanIndex]['target'] = $value->target;
+                    $data[$key - 1]['activities'][$kegiatanIndex]['kerjasama'] = $value->kerjasama;
+                    $data[$key - 1]['activities'][$kegiatanIndex]['ketepatan_waktu'] = $value->ketepatan_waktu;
+                    $data[$key - 1]['activities'][$kegiatanIndex]['kualitas'] = $value->kualitas;
+                } else {
+                    $data[]['employee_name'] = $value->full_name;
+                    $dataIndex = count($data) - 1;
+                    $data[$dataIndex]['nip'] = $value->nip;
+                    $data[$dataIndex]['evaluator_name'] = $value->evaluator_name;
+                    $data[$dataIndex]['activities'][]['activity_name'] = $value->activity_name;
+                    $kegiatanIndex = count($data[$dataIndex]['activities']) - 1;
+                    $data[$dataIndex]['activities'][$kegiatanIndex]['target'] = $value->target;
+                    $data[$dataIndex]['activities'][$kegiatanIndex]['kerjasama'] = $value->kerjasama;
+                    $data[$dataIndex]['activities'][$kegiatanIndex]['ketepatan_waktu'] = $value->ketepatan_waktu;
+                    $data[$dataIndex]['activities'][$kegiatanIndex]['kualitas'] = $value->kualitas;
+                }
+            }
+        }
+
+        $pdf = Pdf::loadView('pages.penilai.pegawai.pdf-template', compact('data'));
+        return $pdf->stream('laporan-nilai-pegawai.pdf');
     }
 }
--- resources/views/pages/penilai/pegawai/pdf-template.blade.php
+++ resources/views/pages/penilai/pegawai/pdf-template.blade.php
@@ -3,159 +3,158 @@
 
 <head>
     <style>
-       
         .styled-table {
             text-align: center;
             width: 100%;
-    border-collapse: collapse;
-    margin: 25px 0;
-    font-size: 0.9em;
-    font-family: sans-serif;
-    min-width: 400px;
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
-}
-.styled-table thead tr {
-    background-color: #009879;
-    color: #ffffff;
-    text-align: left;
-}
-.styled-table th,
-.styled-table td {
-    padding: 12px 15px;
-}
-.styled-table tbody tr {
-    border-bottom: 1px solid #dddddd;
-}
+            border-collapse: collapse;
+            margin: 25px 0;
+            font-size: 0.9em;
+            font-family: sans-serif;
+            min-width: 400px;
+            border: 1px solid #dddddd;
+        }
 
-.styled-table tbody tr:nth-of-type(even) {
-    background-color: #f3f3f3;
-}
+        .styled-table thead tr {
+            background-color: #009879;
+            color: #ffffff;
+            text-align: left;
+        }
 
-.styled-table tbody tr:last-of-type {
-    border-bottom: 2px solid #009879;
-}
-.styled-table tbody tr.active-row {
-    font-weight: bold;
-    color: #009879;
-}
+        .styled-table th,
+        .styled-table td {
+            padding: 12px 15px;
+        }
 
+        .styled-table tbody tr {
+            border-bottom: 1px solid #dddddd;
+        }
+
+        .styled-table tbody tr:nth-of-type(even) {
+            background-color: #f3f3f3;
+        }
+
+        .styled-table tbody tr.active-row {
+            font-weight: bold;
+            color: #009879;
+        }
+
+        .text-center {
+            text-align: center !important;
+        }
+
+        .text-left {
+            text-align: left !important;
+        }
+
+        .w-50 {
+            width: 50%;
+        }
+
+        .w-100 {
+            width: 100%;
+        }
+
+        .page-break {
+            page-break-after: always;
+        }
+        .mb-2 {
+            margin-bottom: 2rem;
+        }
     </style>
 </head>
 
 <body>
-<table>
-        <tr>
-            <td>Nama Pegawai</td>
-            <td>:</td>
-            <td>John Doe</td>
-        </tr>
-        <tr>
-            <td>NIP</td>
-            <td>:</td>
-            <td>1906700042</td>
-        </tr>
-        <tr>
-            <td>Nama Penilai</td>
-            <td>:</td>
-            <td>John Doe</td>
-        </tr>
-        <tr>
-            <td>Perbulan</td>
-            <td>:</td>
-            <td>1-30 agustus</td>
-        </tr>
-        <td> </td>
-        <tr>
-        </tr>
-    </table>
-    <table class="styled-table">
-         <tr>
-            <th>Kegiatan</th>
-            <th>Target</th>
-            <th>Kerjasama</th>
-            <th>Ketepatan Waktu</th>
-            <th>Kualitas</th>
-            <th>Hasil</th>
-        </tr>
-        <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>90</td>
-            <td>90</td>
-            <td>90</td>
-            <td>90</td>
-            <td>90</td>
-        </tr>
-        <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>90</td>
-            <td>90</td>
-            <td>90</td>
-            <td>90</td>
-            <td>90</td>
-        </tr>
-        <tr>
-            <td>Ernst Handel</td>
-            <td>90</td>
-            <td>90</td>
-            <td>90</td>
-            <td>90</td>
-            <td>90</td>
-        </tr>
-        <tr>
-            <td>Island Trading</td>
-             <td>90</td>
-            <td>90</td>
-            <td>90</td>
-            <td>90</td>
-            <td>90</td>
-        </tr>
-        <tr>
-            <td>Laughing Bacchus Winecellars</td>
-             <td>90</td>
-            <td>90</td>
-            <td>90</td>
-            <td>90</td>
-            <td>90</td>
-        </tr>
-        <tr>
-            <td>Magazzini Alimentari Riuniti</td>
-             <td>90</td>
-            <td>90</td>
-            <td>90</td>
-            <td>90</td>
-            <td>90</td>
-        </tr>
-        <tr>
-            <td colspan="5">Rata-rata</td>
-            <td>100</td>
-        </tr>
-        <tr>
-            <td colspan="5">Total</td>
-            <td>100</td>
-        </tr>
-    </table>
+    @foreach ($data as $index => $item)
+        <div>
+            <h1 class="text-center mb-2">Laporan Hasil Kegiatan</h1>
+            <table>
+                <tr>
+                    <td>Nama Pegawai</td>
+                    <td>:</td>
+                    <td>{{ $item['employee_name'] }}</td>
+                </tr>
+                <tr>
+                    <td>NIP</td>
+                    <td>:</td>
+                    <td>{{ $item['nip'] }}</td>
+                </tr>
+                <tr>
+                    <td>Nama Penilai</td>
+                    <td>:</td>
+                    <td>{{ $item['evaluator_name'] }}</td>
+                </tr>
+            </table>
+            <table class="styled-table">
+                <tr>
+                    <th>Kegiatan</th>
+                    <th>Target</th>
+                    <th>Kerjasama</th>
+                    <th>Ketepatan Waktu</th>
+                    <th>Kualitas</th>
+                    <th>Hasil</th>
+                </tr>
+                @php
+                    $rataRata = 0;
+                    $total = 0;
+                @endphp
+                @foreach ($item['activities'] as $activity)
+                    @php
+                        $hasil =($activity['target'] * 40) / 100 + ($activity['kerjasama'] * 10) / 100 + ($activity['ketepatan_waktu'] * 40) / 100 + ($activity['kualitas'] * 10) / 100; 
+                        $total += $hasil;
+                    @endphp
+                    <tr>
+                        <td class="text-left">{{ $activity['activity_name'] }}</td>
+                        <td>{{ $activity['target'] }}</td>
+                        <td>{{ $activity['kerjasama'] }}</td>
+                        <td>{{ $activity['ketepatan_waktu'] }}</td>
+                        <td>{{ $activity['kualitas'] }}</td>
+                        <td>{{ $hasil  }}
+                        </td>
+                    </tr>
+                    @php
+                    @endphp
+                @endforeach
+                <tr>
+                    <td colspan="5">Rata-rata</td>
+                    <td>{{ $total / count($item['activities']) }}</td>
+                </tr>
+                <tr>
+                    <td colspan="5">Total</td>
+                    <td>{{ $total }}</td>
+                </tr>
+            </table>
 
 
 
-    <div style="margin-top: 100px;">
-        <table style="text-align: center; width: 100%;">
-        <tr>
-            <td style="width: 50%;">
-                <p style="line-height: 0;">Purwodadi, 20 Januari 2020</p>
-                <p>Yang bertanda tangan</p>
-        <img src="https://paragram.id/upload/media/entries/2019-07/31/9317-1-7454c0792fd0bc375015427933ee39c4.jpg" width="100px">
+            <div style="margin-top: 100px;">
+                <table class="text-center w-100">
+                    @php
+                        \Carbon\Carbon::setLocale('id');
+                    @endphp
+                    <tr>
+                        <td class="w-50">
+                            <p>Subang, {{ \Carbon\Carbon::now()->isoFormat('D MMMM Y') }}</p>
+                            <p style="margin-top: -15px">Yang bertanda tangan</p>
+                            <img src="https://paragram.id/upload/media/entries/2019-07/31/9317-1-7454c0792fd0bc375015427933ee39c4.jpg"
+                                width="100px">
 
-                <p>Arbrian Abdul Jamal</p>
-            </td>
-            <td style="width: 50%;">
-                <p style="line-height: 0;">&nbsp;</p>
-                <p>Yang bertanda tangan</p>
-        <img src="https://paragram.id/upload/media/entries/2019-07/31/9317-1-7454c0792fd0bc375015427933ee39c4.jpg" width="100px">
-                <p>Arbrian Abdul Jamal</p>
-            </td>
-        </tr>
-    </table>
-    </div>
+                            <p>Arbrian Abdul Jamal</p>
+                        </td>
+                        <td class="w-50">
+                            <p>&nbsp;</p>
+                            <p style="margin-top: -15px">Yang bertanda tangan</p>
+                            <img src="https://paragram.id/upload/media/entries/2019-07/31/9317-1-7454c0792fd0bc375015427933ee39c4.jpg"
+                                width="100px">
+                            <p>Arbrian Abdul Jamal</p>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        @if (!$loop->last)
+        <div class="page-break"></div>
+        @endif
+    @endforeach
 </body>
 
 </html>
